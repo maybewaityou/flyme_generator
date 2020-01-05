496cc011554c5d00108ee1753916735ad4a521e8
MeePwn <maybewaityou@gmail.com>
2020-01-05 18:41:42 +0800

    first commit

---
+++ lib/test/route_configs.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'route_configs.dart';
+
+// **************************************************************************
+// RouteGenerator
+// **************************************************************************
+
+// FirstPage * /home/first;
+
+// SecondPage * /home/second;
+
+// **************************************************************************
+// FlymeGenerator
+// **************************************************************************
+
+// Source library: package:flyme_generator/test/route_configs.dart
+const topLevelNumVarCount = -1;
---
+++ lib/test/test.g.dart
@@ -0,0 +1,10 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'test.dart';
+
+// **************************************************************************
+// FlymeGenerator
+// **************************************************************************
+
+// Source library: package:flyme_generator/test/test.dart
+const topLevelNumVarCount = -1;
