e033c47ff9e9eb88eab557d6102758c65f25b0b3
MeePwn <dong.chunnan@chinamworld.com>
2020-07-13 09:07:23 +0800

    first commit

--- example/ddd/domain_registry.dart
+++ example/ddd/domain_registry.dart
@@ -3,4 +3,4 @@ import 'package:flyme_annotation/flyme_annotation.dart';
 part 'domain_registry.g.dart';
 
 @DomainFactory()
-class DomainRegistry {}
+class DomainRegistry extends _$DomainRegistry {}
---
+++ example/ddd/view_model.dart
@@ -0,0 +1,11 @@
+import 'package:flyme_annotation/flyme_annotation.dart';
+
+part 'view_model.g.dart';
+
+@Properties([])
+class ViewModel extends _$ViewModel {
+  @override
+  void init() {
+    print('==== ViewModel init ====');
+  }
+}
